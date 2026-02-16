bc9c8263815180b6bf1d4df074dd3369945ef88c
João Monteiro <114676364+joaofcmont@users.noreply.github.com>
2026-02-16 15:37:15 +0000

    Start of project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("What you want to do? (add/list/summary/delete)");
+
+    }
+}
